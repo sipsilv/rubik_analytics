5b7f5baf56ac339d26c18656bc98be7d0173b58b
santhosh <santhoshjallu@gmail.com>
2026-01-05 01:18:55 -0800

    adjusted it for production code

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,22 +3,10 @@ const path = require('path')
 
 const nextConfig = {
   reactStrictMode: true,
-<<<<<<< HEAD
-  output: 'standalone',
-  eslint: {
-    // Disable ESLint during production builds
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Disable TypeScript errors during production builds
-    ignoreBuildErrors: true,
-  },
-=======
 
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../'),
   }
->>>>>>> 8e1afed (adjusted it for production code)
 }
 
 module.exports = nextConfig
